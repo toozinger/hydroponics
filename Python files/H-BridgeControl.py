18eb2f0da208c2d1e29b640140f2a88c548ed1ac
Teal Dowd <toozinger@gmail.com>
2016-11-10 00:00:58 -0500

    Creates functional program for H-bridge control
    This simple script will run a motor or light in
    both directions by flipping polarities. Simple implementation.

--- Python files/H-BridgeControl.py
+++ Python files/H-BridgeControl.py
@@ -1,50 +1,42 @@
-# Import required libraries
+# This code created to run the L298N H bridge
+# Put jumper over enable pins (sideways pins)
+# Else, use PWM on those pins
+# IN1 - IN4 are set high to make motor x go direction y
+# 12v is where power goes in. Remember common grounds
 
+# Import required libraries
 import sys
 import time
 import RPi.GPIO as GPIO
 
-# Use BCM GPIO references
-# instead of physical pin numbers
-#GPIO.setmode(GPIO.BCM)
+# Use BCM GPIO reference (physical pin #)
 mode=GPIO.getmode()
-print " mode ="+str(mode)
-#GPIO.cleanup()
 
-# Define GPIO signals to use
-# Physical pins 11,15,16,18
-# GPIO17,GPIO22,GPIO23,GPIO24
-
-StepPinForward=16
-StepPinBackward=18
-goPin = 11
+# Define motor pins
+StepPinForward=38
+StepPinBackward=40
 sleeptime=1
 
+# Set GPIO pins as out
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(StepPinForward, GPIO.OUT)
 GPIO.setup(StepPinBackward, GPIO.OUT)
-GPIO.setup(goPin, GPIO.OUT)
 
-
+# Simple def to go forward
 def forward(x):
-    GPIO.output(goPin, GPIO.HIGH)
     GPIO.output(StepPinForward, GPIO.HIGH)
-    GPIO.output(StepPinBackward, GPIO.LOW)
     print "forwarding running  motor "
     time.sleep(x)
     GPIO.output(StepPinForward, GPIO.LOW)
-    GPIO.output(goPin, GPIO.LOW)
 
 def reverse(x):
-    GPIO.output(goPin, GPIO.HIGH)
     GPIO.output(StepPinBackward, GPIO.HIGH)
-    GPIO.output(StepPinForward, GPIO.LOW)
     print "backwarding running motor"
     time.sleep(x)
     GPIO.output(StepPinBackward, GPIO.LOW)
-    GPIO.output(goPin, GPIO.LOW)
 
-for i in range(2):
+# Flip flop 1000 times
+for i in range(100):
     print "forward motor "
     forward(2)
     print "reverse motor"
